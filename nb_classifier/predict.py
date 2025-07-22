9ddc9b4bc16d57f1aab90f3c57e630448a8d42da
Nguyen Ngoc <knkidngoc@gmail.com>
2025-07-22 18:36:54 +0700

    refactor predict.py

--- nb_classifier/predict.py
+++ nb_classifier/predict.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import numpy as np
 import pickle
